093dc8b2c76b3b26df335c2f34348f5fc7d94336
Alexander BORISOV <67188470+syzygyexe@users.noreply.github.com>
2020-08-30 13:53:10 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
+import Sidebar from "./Sidebar"
+import Chat from "./Chat"
 
 function App() {
   return (
     <div className="app">
       <div className="app_body">
-    )
+        <Sidebar />
+        <Chat />
+      </div>
+    </div>
+  );
 }
+
+export default App;
